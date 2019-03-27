ac6553b6b348b917a332d1642ce72b8d840b153e
madosuki <yurikasamaaikatsu@gmail.com>
2019-03-27 15:41:05 +0900

    change message format

--- main.lisp
+++ main.lisp
@@ -4,4 +4,4 @@
   (let ((a (read))
         (b (read)))
     (let ((result (multiple-value-list (extend-gcd a b))))
-      (format t "~a * ~a + ~a * ~a = ~a~%" a (nth 0 result) b (nth 1 result) (my-gcd a b)))))
+      (format t "(~a * ~a) + (~a * ~a) = ~a~%" a (car result) b (cadr result) (my-gcd a b)))))
